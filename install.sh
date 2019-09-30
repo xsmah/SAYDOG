1e257d0e5d1fa62ce089ceb3d68f7df767279784
-=[ Say Dog ]=- <55991841+saydog@users.noreply.github.com>
2019-09-30 15:42:38 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg update && pkg upgrade
+pkg install python python2
+pip2 install mechanize
+pip2 install requests
+pkg install ruby && gem install lolcat
+pkg install openssh
+pkg install openssl
+pkg install wget
+pkg install curl
+pkg install php
+pkg install figlet
+pkg install screenfetch
+
