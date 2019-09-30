b45ba821c59b6186796273ceea9d4cfb0141ba3e
-=[ Say Dog ]=- <55991841+saydog@users.noreply.github.com>
2019-09-30 15:49:21 +0000

    Add files via upload

---
+++ v1/show.sh
@@ -0,0 +1,444 @@
+#usr/bin/bash
+r='\033[31;1m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;1m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+d='\033[90m' #abugelap
+
+# [Coded By] Iqbalmh18
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $y"                  -=["$w"+"$y"]=-"$w" DAFTAR TOOLS "$y"-=["$w"+"$y"]=-"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"-----"$w" TERMUX TOOLS"$r" -----"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  1."$w" Termux Logo"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  2."$w" Termux World Maps"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  3."$w" Termux Cursor"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  4."$w" Termux Fire Animation"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  5."$w" Termux Matrix Animation"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  6."$w" Termux Color & Fonts"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  7."$w" Termux Ohmyzsh"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  8."$w" Termux Game"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r"  9."$w" Termux Google"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+sleep 0.1
+echo $r" 10."$w" Termux Tutorial"
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+echo
+sleep 0.1
+echo $d"---------------------------------------------------------------"
+echo $r"-----"$w" HACK WEBSITE TOOLS"$r" -----"
+echo $d"---------------------------------------------------------------"
+echo $r" 11."$w" Admin Finder"
+echo $d"---------------------------------------------------------------"
+echo $r" 12."$w" Script Deface Creators"
+echo $d"---------------------------------------------------------------"
+echo $r" 13."$w" Auto Deface Web"
+echo $d"---------------------------------------------------------------"
+echo $r" 14."$w" Lite Script"
+echo $d"---------------------------------------------------------------"
+echo $r" 15."$w" SQL-Maps"
+echo $d"---------------------------------------------------------------"
+echo
+echo $d"---------------------------------------------------------------"
+echo $r"-----"$w" HACK FACEBOOK TOOLS"$r" -----"
+echo $d"---------------------------------------------------------------"
+echo $r" 16."$w" Dark Facebook versi 1.7"
+echo $d"---------------------------------------------------------------"
+echo $r" 17."$w" Dark Facebook versi 1.8"
+echo $d"---------------------------------------------------------------"
+echo $r" 18."$w" Dark Facebook versi 1.9"
+echo $d"---------------------------------------------------------------"
+echo $r" 19."$w" Open Source Information Facebook"
+echo $d"---------------------------------------------------------------"
+echo $r" 20."$w" Bruteforce Facebook"
+echo $d"---------------------------------------------------------------"
+echo
+echo $d"---------------------------------------------------------------"
+echo $r"-----"$w" PHISING TOOLS"$r" -----"
+echo $d"---------------------------------------------------------------"
+echo $r" 21."$w" Shellphis"
+echo $d"---------------------------------------------------------------"
+echo $r" 22."$w" Weeman"
+echo $d"---------------------------------------------------------------"
+echo $r" 23."$w" PulsaKuota"$y" serveo"
+echo $d"---------------------------------------------------------------"
+echo
+echo $d"---------------------------------------------------------------"
+echo $r"-----"$w" DDOS TOOLS"$r" -----"
+echo $d"---------------------------------------------------------------"
+echo $r" 24."$w" Dos Tookis"
+echo $d"---------------------------------------------------------------"
+echo $r" 25."$w" Hammer"
+echo $d"---------------------------------------------------------------"
+echo $r" 26."$w" Lucita Ddos"
+echo $d"---------------------------------------------------------------"
+echo
+echo $d"---------------------------------------------------------------"
+echo $r"-----"$w" SPAMMER TOOLS"$r" -----"
+echo $d"---------------------------------------------------------------"
+echo $r" 27."$w" Brutal Sms"
+echo $d"---------------------------------------------------------------"
+echo $r" 28."$w" Brutal Call"
+echo $d"---------------------------------------------------------------"
+echo $r" 29."$w" Spam Grab"
+echo $d"---------------------------------------------------------------"
+echo $r" 30."$w" Spammer"
+echo $d"---------------------------------------------------------------"
+echo $y""
+read -p"<<< SAYDOG®INPUT >>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+pkg install screenfetch
+clear
+echo $r"["$w"1"$r"]" $w"screenfetch -A Linux"
+screenfetch -A Linux
+echo $r"["$w"2"$r"]" $w"screenfetch -A Debian"
+screenfetch -A Debian
+echo $r"["$w"3"$r"]" $w"screenfetch -A OpenSUSE"
+screenfetch -A OpenSUSE
+echo $r"["$w"4"$r"]" $w"screenfetch -A Trisquel"
+screenfetch -A Trisquel
+echo $r"["$w"5"$r"]" $w"screenfetch -A Korora"
+screenfetch -A Korora
+echo $r"["$w"6"$r"]" $w"screenfetch -A SiWanna"
+screenfetch -L SiWanna
+echo $r"["$w"7"$r"]" $w"screenfetch -A SteamOS"
+screenfetch -A SteamOS
+echo $r"["$w"8"$r"]" $w"screenfetch -A Devuan"
+screenfetch -A Devuan
+echo $r"["$w"9"$r"]" $w"screenfetch -A Frugalware"
+screenfetch -A Frugalware
+echo $r"["$w"10"$r"]" $w"screenfetch -A NixOS"
+screenfetch -A NixOS
+echo $r"["$w"11"$r"]" $w"screenfetch -A SparkyLinux"
+screenfetch -A SparkyLinux
+echo $r"["$w"12"$r"]" $w"screenfetch -A CentOS"
+screenfetch -A CentOS
+echo $r"["$w"13"$r"]" $w"screenfetch -A BunsenLabs"
+screenfetch -A BunsenLabs
+echo $r"["$w"14"$r"]" $w"screenfetch -A Fedora"
+screenfetch -A Fedora
+echo $r"["$w"15"$r"]" $w"screenfetch -A Netrunner"
+screenfetch -A Netrunner
+echo $r"["$w"16"$r"]" $w"screenfetch -A Mandriva"
+screenfetch -A Mandriva
+echo $r"["$w"17"$r"]" $w"screenfetch -A BLAG"
+screenfetch -A BLAG
+echo $r"["$w"18"$r"]" $w"screenfetch -A Slackware"
+screenfetch -A Slackware
+echo $r"["$w"19"$r"]" $w"screenfetch -A Vipper and Void"
+screenfetch -A Vipper and Void
+echo $r"["$w"20"$r"]" $w"screenfetch -A SailfishOS"
+screenfetch -A SailfishOS
+echo $r"["$w"21"$r"]" $w"screenfetch -A ROSA"
+screenfetch -A ROSA
+echo $r"["$w"22"$r"]" $w"screenfetch -A Pardus"
+screenfetch -A Pardus
+echo $r"["$w"23"$r"]" $w"screenfetch -A KaOS"
+screenfetch -A KaOS
+echo
+echo $r"["$w"+"$r"]" $w"Screenfetch ini bisa kalian pasang di halaman utama Termux dengan masuk ke direktori profile/zshrc kemudian masukan nama screenfetch di atas"
+echo $w"Jika ingin menambahkan warna yang lebih, ketik saja :" $y"| lolcat"
+echo $r"Contoh" $w":"
+echo
+echo $w"screenfetch -A SteamOS "$y"|" $w"lolcat"
+echo $w"date "$y"|" $w"lolcat"
+echo
+echo $w"Hasilnya" $y":"
+screenfetch -A SteamOS | lolcat
+date | lolcat
+fi
+
+if [ $pil = 2 ]
+then
+clear
+telnet mapscii.me
+fi
+
+if [ $pil = 3 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/Funkyz86/fun-project
+cd fun-project
+python2 fun-project.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+pkg install libcaca
+cacafire
+fi
+
+if [ $pil = 5 ]
+then
+clear
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $pil = 6 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+cd termux-ohmyzsh
+sh install.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+nano ~/.zshrc
+fi
+
+if [ $pil = 8 ]
+then
+clear
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $pil = 9 ]
+then
+clear
+pkg install w3m
+w3m www.google.com
+fi
+
+if [ $pil = 10 ]
+then
+clear
+xdg-open https://m.youtube.com/c/PengangguranMuda
+fi
+
+if [ $pil = 11 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/mrcakil/admin_finder
+cd admin_finder
+php adfin.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/Ubaii/Script-Deface-Creators
+cd Script-Deface-Creators
+python2 create.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/Ranginang67/AOXdeface
+cd AOXdeface
+python2 aox.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+chmod +x LITESCRIPT.py
+python2 LITESCRIPT.py
+fi
+
+if [ $pil = 15 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python sqlmap.py
+fi
+
+if [ $pil = 16 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/darkfb/terbaru
+cd terbaru
+python2 darkfb.pyc
+fi
+
+if [ $pil = 17 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/Mr-XsZ/Dark-Fb
+cd Dark-Fb
+sh install.sh
+fi
+
+if [ $pil = 18 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/pengangguranmuda/DARK-FACEBOOK
+cd DARK-FACEBOOK
+sh install.sh
+fi
+
+if [ $pil = 19 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/CiKu370/OSIF
+cd OSIF
+python2 osif.py
+fi
+
+if [ $pil = 20 ]
+then
+clear
+cd ~/SAYDOG/v1
+curl -S https://pastebin.com/raw/yUpg8zsv --output fb.py
+python2 fb.py
+fi
+
+if [ $pil = 21 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/thelinuxchoice/shellphis
+cd shellphis
+bash shellphis.sh
+fi
+
+if [ $pil = 22 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+
+if [ $pil = 23 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/viruz09/Pulsakuota
+cd Pulsakuota
+unzip Pulsakuota.zip
+cd ~/Pulsakuota
+php -S localhost:8080 -t Pulsakuota
+fi
+
+if [ $pil = 24 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/BPCATTACKER/DOS-TOOKIS
+cd DOS-TOOKIS
+sh DOS-TOOKIS.SH
+fi
+
+if [ $pil = 25 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python2 hammer.py
+fi
+
+if [ $pil = 26 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+python pukul.py
+fi
+
+if [ $pil = 27 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 28 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/LOolzeC/CALL-BRUTAL
+cd CALL-BRUTAL
+python2 call.py
+fi
+
+if [ $pil = 29 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+echo "Username : Anak"
+echo "Password : Bangsa"
+fi
+
+if [ $pil = 30 ]
+then
+clear
+cd ~/SAYDOG/v1
+git clone https://github.com/pengangguranmuda/SPAMMER
+cd SPAMMER
+sh install.sh
+fi
+
+if [ $pil = keluar ]
+then
+echo $r"[!]"$w" Exiting Program !"
+echo $r"[!]"$w" Bye bye ..."
+exit
+fi
+
